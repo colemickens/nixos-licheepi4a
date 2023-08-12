b4468555b53ce3da68b535c185cdd881b7e90f9a
Cole Mickens <cole.mickens@gmail.com>
2023-08-12 17:31:28 -0500

    firmware: pin to stable rev

--- pkgs/firmware/light_aon_fpga.nix
+++ pkgs/firmware/light_aon_fpga.nix
@@ -4,11 +4,11 @@
 }:
 stdenv.mkDerivation {
   pname = "light_aon_fpga-firmware";
-  version = "unstable-2023-06-25";
+  version = "unstable-2023-05-11";
 
   src = fetchurl {
-    url = "https://github.com/chainsx/fedora-riscv-builder/raw/20230623-0255/firmware/light_aon_fpga.bin";
-    hash = "sha256-cIFaWDbiX0nNzlhkhhKapjGeN64dblFE1DxZh01kV5s=";
+    url = "https://github.com/chainsx/fedora-riscv-builder/raw/4230d08f703e4b53ecaac530ecbd4b74e50d22d4/firmware/light_aon_fpga.bin";
+    hash = "sha256-cIFaWDbiX0nNzlhkhhKapjGeN64dblFE1DxZh01kxxx=";
   };
 
   buildCommand = ''
--- pkgs/firmware/light_c906_audio.nix
+++ pkgs/firmware/light_c906_audio.nix
@@ -2,13 +2,15 @@
   stdenv,
   fetchurl,
 }:
+
+le
 stdenv.mkDerivation {
   pname = "light_c906_audio-firmware";
-  version = "unstable-2023-06-25";
+  version = "unstable-2023-05-11";
 
   src = fetchurl {
-    url = "https://github.com/chainsx/fedora-riscv-builder/raw/20230623-0255/firmware/light_c906_audio.bin";
-    hash = "sha256-hv9OQhc6cH7swqVrhPhIxgUsgH4JMiOjw+DRWcK9nNQ=";
+    url = "https://github.com/chainsx/fedora-riscv-builder/raw/4230d08f703e4b53ecaac530ecbd4b74e50d22d4/firmware/light_c906_audio.bin";
+    hash = "sha256-hv9OQhc6cH7swqVrhPhIxgUsgH4JMiOjw+DRWcKyyyy=";
   };
 
   buildCommand = ''
